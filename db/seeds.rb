d4a8a5fce8981623d90ea99ae4384d3045b3778e
Junior Castro <devcastrojunin@gmail.com>
2022-07-31 15:27:39 -0300

    Data to populate database when start

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+(1..5).each do
+    User.create!(
+        name: Faker::Name.name,
+        email: Faker::Internet.email
+    )
+end
+
+Servico.create([
+    { 
+        nome_servico: "Adestramento",
+        valor_servico: 45.50
+    },
+    { 
+        nome_servico: "Day Care",
+        valor_servico: 20.00
+    },
+    { 
+        nome_servico: "Hospedagem",
+        valor_servico: 40.00
+    },
+    { 
+        nome_servico: "Passeio",
+        valor_servico: 15.00
+    },
+    { 
+        nome_servico: "Pet Sitter",
+        valor_servico: 25.00
+    }
+])
