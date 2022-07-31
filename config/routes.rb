4829b815ea0fe0989e0ebac178f1a3a1cbdbb0b2
Junior Castro <devcastrojunin@gmail.com>
2022-07-31 15:26:20 -0300

    New routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resources :users
       resources :servicos
+      resources :orders
+      get '/user_orders/:user_id', to: 'orders#orders_by_user_id'
     end
   end
 end
