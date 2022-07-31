7aed548b9638e3637f42de5f407b5cfe8812f657
Junior Castro <devcastrojunin@gmail.com>
2022-07-31 15:27:16 -0300

    Change locale data

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module PetAnjoApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.i18n.available_locales = ['es-CO', :ptBR]
+    config.i18n.available_locales = ['en', :ptBR]
     config.i18n.default_locale = :ptBR
   end
 end
